e946108470a00f54e6ea823835039f41be9284bd
Alexander Missaoui <alexandermissaoui@gmail.com>
2023-09-14 13:47:34 +0200

    styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,8 @@ const Header = () => {
   return (
     <div>
       <div className='Header'>
-        <a href="/" className='header-link'><h1>Web Shop</h1></a>
-        <a href='/post' id='create-btn'>Create New Product</a>
+        <a href="/" className='header-link'><h1>Carbuy.com</h1></a>
+        <a href='/post' id='create-btn'>Publish New Car</a>
       </div>
     </div>
   )
