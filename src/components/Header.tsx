289344e98f5ccc283d3e1922d26c7e870e277ee4
Alexander Missaoui <alexandermissaoui@gmail.com>
2023-09-15 10:30:20 +0200

    cart button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,8 @@ const Header = () => {
       <div className='Header'>
         <a href="/" className='header-link'><h1>Carbuy.com</h1></a>
         <a href='/post' id='create-btn'>Publish New Car</a>
+        <a href='/Cart' id='create-btn'>Shopping Cart</a>
+
       </div>
     </div>
   )
